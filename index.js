47800671dada1f89d6b53aece5bdc8e96481b42b
Maggie Saperstein <maggie.saperstein@gmail.com>
2022-05-19 08:33:04 -0500

    commit tree e94fbd5b23fc881c0ad4762f2da6947eac9d0df6

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+  return customerName;
+
+}
+
+function setBestCustomer(){
+  bestCustomer = 'not bob';
+  return bestCustomer;
+}
+
+function overwriteBestCustomer(){
+  bestCustomer = 'maybe bob';
+  return bestCustomer;
+}
+
+const leastFavoriteCustomer = 'nancy';
+
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = 'greg';
+}
